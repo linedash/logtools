4120e83480473942943463fe49e285e64784b187
linedash <linedash@users.noreply.github.com>
2016-08-30 17:12:50 +0100

    Autodetect webroot

--- logfilter.py
+++ logfilter.py
@@ -12,6 +12,7 @@ import socket
 import subprocess
 import sys
 
+import findhost
 
 # Rough and ready regex for parsing log lines.
 LOG_LINE = re.compile(
@@ -107,19 +108,51 @@ def geoip_parse(line):
 
 
 def main():
-    if len(sys.argv) < 2:
-        print >> sys.stderr, "Please provide a webroots path."
+    #auto-detect host type and set webroot accordingly.  Also accept from first argument
+    host_type = findhost.get_host_type(socket.getfqdn())
+    if host_type == "linweb":
+        webroots = sys.argv[1] if len(sys.argv) >= 2 else "/usr/local/pem/vhosts/"
+    elif host_type == "lwng":
+        webroots = sys.argv[1] if len(sys.argv) >= 2 else findlwngwebroot()
+    elif host_type == "other":
+        print >> sys.stderr, "Invalid host type for script"
         return 1
 
-    # Get the webroots path as first argument
-    webroots = sys.argv[1]
+    #if len(sys.argv) < 2:
+    #    print >> sys.stderr, "Please provide a webroots path."
+    #    return 1
+
     # Get the GeoIP database path as the second argument, if present.
-    geoip_db = 'GeoIP.dat' if len(sys.argv) == 2 else sys.argv[2]
+    geoip_db = sys.argv[2] if len(sys.argv) >= 3 else "GeoIP.dat"
 
     for item in filter_logs(follow_logs(glob_logs(webroots)), geoip_db):
         print "%(cc)s %(ip)s %(method)s %(uri)s" % item
 
     return 0
+
+
+
+r'''
+I don't care if this is done wrong.  Leave it alone.
+'''
+
+def findlwngwebroot():
+    p1 = subprocess.Popen(["redis-cli", '-s', '/var/lib/redis/redis.sock', 'KEYS', 'website-by-name:*'], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["tail", "-1"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    output,err = p2.communicate()
+    p2.stdout.close()
+    p3 = subprocess.Popen(["redis-cli", '-s', '/var/lib/redis/redis.sock', 'get', output.strip()], stdout=subprocess.PIPE)
+    p4 = subprocess.Popen(['redis-decode-obj', '--type=vh_info_t'], stdin=p3.stdout, stdout=subprocess.PIPE)
+    p3.stdout.close()
+    p5 = subprocess.Popen(['awk', '/m_homedir/ { gsub(/"/,"") ; print $2 }'], stdin=p4.stdout, stdout=subprocess.PIPE)
+    p4.stdout.close()
+    output2,err = p5.communicate()
+    p5.stdout.close()
+    lwng_webroot_random_string = output2.split('/',5)[4]
+    lwngwebroot = "/var/www/vhosts/" + lwng_webroot_random_string
+    return lwngwebroot
+
 
 
 if __name__ == '__main__':
